70b5b77d445df913574392632d5763bac30b50e5
João Pedro FF <joaofrancaf@hotmail.com>
2023-04-27 23:45:30 -0300

    'first'

---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import {globalCss} from "./stitches.config"
+
+export const globaStyles = globalCss ({
+    /* default css */
+    '*': {
+        margin: 0,
+        padding: 0,
+        boxSizing: 'border-box'
+    },
+
+    body: {
+        backgroundColor: '$--base---background',
+        color: '$--base---text',  /* texto cor */
+        '-webkit-font-smoothing' : 'antialiased',
+    },
+
+    'body, input, textarea, button': {
+        fontFamily: 'Roboto',
+        fontWeight: 400,
+    },
+
+    
+})
