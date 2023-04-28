0a635ec33c6456c1b8e5f7bbcb4daa45fba41dc8
joão.frança <joaofrancaf@hotmail.com>
2023-04-28 15:24:34 -0300

    a

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,20 +1,23 @@
-import { DivHeaderFeatureStyled, DivHeaderStyled, DivHeaderTextStyled } from "./styles";
+import { DivHeaderFeatureStyled, DivHeaderH1Styled, DivHeaderPStyled, DivHeaderStyled, DivHeaderTextStyled } from "./styles";
 import Imgcafe from '../../assets/Imagemcafe.png'
+import { ShoppingCart } from '@phosphor-icons/react'
 
 export default function Homepage() {
     return (
         <DivHeaderStyled>
             <DivHeaderTextStyled>
-                <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-                <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+                <DivHeaderH1Styled>
+                    <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+                </DivHeaderH1Styled>
+                <DivHeaderPStyled>
+                    <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+                </DivHeaderPStyled>
+                <DivHeaderFeatureStyled>
+                    <ShoppingCart/> <span>Compra simples e segura</span>
+                </DivHeaderFeatureStyled>
             </DivHeaderTextStyled>
 
-            <DivHeaderFeatureStyled>
-                
-            </DivHeaderFeatureStyled>
-
             <img src={Imgcafe}/>
-            
         </DivHeaderStyled>
     )
 }
--- src/pages/Homepage/styles.ts
+++ src/pages/Homepage/styles.ts
@@ -2,9 +2,20 @@ import { styled } from "../../styles/stitches.config";
 
 export const DivHeaderStyled = styled('div', {
     height: '34rem',
+
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
 })
 
 export const DivHeaderTextStyled = styled('div', {
+    height: ''
+})
+
+export const DivHeaderH1Styled = styled('div', {
+})
+
+export const DivHeaderPStyled = styled('div', {
 })
 
 export const DivHeaderFeatureStyled = styled('div', {
