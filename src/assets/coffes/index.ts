9e1f784a227470fd06623c345145c54def6e0258
João Pedro FF <joaofrancaf@hotmail.com>
2023-05-17 22:46:15 -0300

    trying-to-fix-imagens-on-production

---
+++ src/assets/coffes/index.ts
@@ -0,0 +1,31 @@
+import ExpressoTradicional from './Image.png';
+import ExpressoAmericano from './Image-1.png';
+import ExpressoCremoso from './Image-2.png';
+import ExpressoGelado from './Image-3.png';
+import CafecomLeite from './Image-4.png';
+import Latte from './Image-5.png';
+import Capuccino from './Image-6.png';
+import Macchiato from './Image-7.png';
+import Mocaccino from './Image-8.png';
+import ChocolateQuente from './Image-9.png';
+import Cubano from './Image-10.png';
+import Havaiano from './Image-11.png';
+import Arabe from './Image-12.png';
+import Irlandes from './Image-13.png';
+
+export {
+  ExpressoTradicional,
+  ExpressoAmericano,
+  ExpressoCremoso,
+  ExpressoGelado,
+  CafecomLeite,
+  Latte,
+  Capuccino,
+  Macchiato,
+  Mocaccino,
+  ChocolateQuente,
+  Cubano,
+  Havaiano,
+  Arabe,
+  Irlandes
+};
