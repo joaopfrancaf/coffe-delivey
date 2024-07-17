8af68c47f168e5406ed7ac53adb2857815ebfeee
joao pedro <joaopfrancaf@gmail.com>
2024-07-17 01:22:37 -0300

    web notifications trying

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,32 @@ createServer({
   },
 })*/
 
+window.Notification.requestPermission(permission => {
+})
+
+navigator.serviceWorker.register("service-worker.js")
+  .then(async serviceWorker => {
+    let subs = await serviceWorker.pushManager.getSubscription()
+
+    if(!subs) {
+      const publicKeyRes = await fetch("https://coffe-api-nextjs.vercel.app/api/push/public_key")
+      
+      const publicKey = await publicKeyRes.json();
+      
+      subs = await serviceWorker.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: publicKey
+      });
+    }
+
+    await fetch("https://coffe-api-nextjs.vercel.app/api/push/register", {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify(subs)
+    })
+  })
 
 export default function App() {
 
